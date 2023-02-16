e1190b62dbd142b5555ab06129e43c84ad6b3056
Naveen Rajan <naveentherocker1234@gmail.com>
2023-02-17 01:50:40 +0530

    Callbacks

--- main.js
+++ main.js
@@ -17,23 +17,69 @@ let rtcClient;
 
 const initRtc = async () => {
   rtcClient = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
-  
+  rtcClient.on('user-joined', handleUserJoined)
+  rtcClient.on('user-published', handleUserPublished)
+  rtcClient.on('user-left', handleUserLeft)
+
+
+
   await rtcClient.join(appid, roomId, token, rtcUid)
 
   audioTracks.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
   await rtcClient.publish(audioTracks.localAudioTrack);
 
-  document.getElementById('members').insertAdjacentHTML('beforeend', `<div class="speaker user-rtc-${rtcUid}" id="${rtcUid}"><p>${rtcUid}</p></div>`)
+  let userWrapper = `<div class="speaker user-rtc-${rtcUid}" id="${rtcUid}"><p>${rtcUid}</p></div>`
+
+  document.getElementById('members').insertAdjacentHTML('beforeend', userWrapper)
+
+
+}
+
+let handleUserJoined = async (user) => {
+  console.log('A NEW USER HAS JOINED:', user)
+
+  let userWrapper = `<div class="speaker user-rtc-${user.uid}" id="${user.uid}"><p>${user.uid}</p></div>`
+
+  document.getElementById('members').insertAdjacentHTML('beforeend', userWrapper)
+
+}
+
+let handleUserPublished = async (user, mediaType) => {
+  await rtcClient.subscribe(user, mediaType)
+
+  if(mediaType == 'audio'){
+    audioTracks.remoteAudioTracks[user.uid] = [user.audioTrack]
+    user.audioTrack.play()
+  }
+}
+
+let handleUserLeft = async (user) => {
+  delete audioTracks.remoteAudioTracks[user.uid]
+  document.getElementById(user.uid).remove()
 }
 
 let lobbyForm = document.getElementById('form')
 
 const enterRoom = async (e) => {
   e.preventDefault()
-  initRtc()
+  initRtc() 
 
   lobbyForm.style.display = 'none'
   document.getElementById('room-header').style.display = "flex"
 }
 
+let leaveRoom = async () => {
+  audioTracks.localAudioTrack.stop()
+  audioTracks.localAudioTrack.close()
+
+  rtcClient.unpublish()
+  rtcClient.leave()
+
+  document.getElementById('form').style.display = 'block'
+  document.getElementById('room-header').style.display = 'none'
+  document.getElementById('members').innerHTML = ''
+
+}
+
 lobbyForm.addEventListener('submit', enterRoom)
+document.getElementById('leave-icon').addEventListener('click', leaveRoom)
