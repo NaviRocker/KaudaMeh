f8a479bba582ae455ffbf66f3044fdfd320edb8b
Naveen Rajan <naveentherocker1234@gmail.com>
2023-02-19 13:39:09 +0530

    Configure RTM

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import './style.css'
 import AgoraRTC from "agora-rtc-sdk-ng"
+import AgoraRTM from 'agora-rtm-sdk'
 
 const appid = "33e8d887b5c84942a8b07b2e514a9fba"
 const token = null
 const rtcUid =  Math.floor(Math.random() * 2032)
+const rtmUid =  String(Math.floor(Math.random() * 2032))
+
 
 let roomId = "main"
 
@@ -13,13 +16,15 @@ let audioTracks = {
 };
 
 let rtcClient;
+let rtmClient;
+let channel;
 
 let micMuted = true
 
 
 const initRtc = async () => {
   rtcClient = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
-  rtcClient.on('user-joined', handleUserJoined)
+  // rtcClient.on('user-joined', handleUserJoined)
   rtcClient.on('user-published', handleUserPublished)
   rtcClient.on('user-left', handleUserLeft)
 
@@ -29,11 +34,11 @@ const initRtc = async () => {
   audioTracks.localAudioTrack.setMuted(micMuted)
   await rtcClient.publish(audioTracks.localAudioTrack);
 
-  let userWrapper = `<div class="speaker user-rtc-${rtcUid}" id="${rtcUid}"><p>${rtcUid}</p></div>`
+  // let userWrapper = `<div class="speaker user-rtc-${rtcUid}" id="${rtcUid}"><p>${rtcUid}</p></div>`
 
-  document.getElementById('members').insertAdjacentHTML('beforeend', userWrapper)
+  // document.getElementById('members').insertAdjacentHTML('beforeend', userWrapper)
 
-  initVolumeIndicator()
+  // initVolumeIndicator()
 }
 
 let initVolumeIndicator = async () => {
@@ -61,15 +66,15 @@ let initVolumeIndicator = async () => {
   })
 }
 
-let handleUserJoined = async (user) => {
-  console.log('A NEW USER HAS JOINED:', user)
+// let handleUserJoined = async (user) => {
+  
 
-  let userWrapper = `<div class="speaker user-rtc-${user.uid}" id="${user.uid}"><p>${user.uid}</p></div>`
+//   let userWrapper = `<div class="speaker user-rtc-${user.uid}" id="${user.uid}"><p>${user.uid}</p></div>`
 
-  document.getElementById('members').insertAdjacentHTML('beforeend', userWrapper)
+//   document.getElementById('members').insertAdjacentHTML('beforeend', userWrapper)
 
   
-}
+// }
 
 let handleUserPublished = async (user, mediaType) => {
   await rtcClient.subscribe(user, mediaType)
@@ -82,7 +87,7 @@ let handleUserPublished = async (user, mediaType) => {
 
 let handleUserLeft = async (user) => {
   delete audioTracks.remoteAudioTracks[user.uid]
-  document.getElementById(user.uid).remove()
+  // document.getElementById(user.uid).remove()
 }
 
 let toggleMic = async (e) => {
